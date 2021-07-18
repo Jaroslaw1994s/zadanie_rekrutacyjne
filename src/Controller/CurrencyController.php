e6fcfaa0b4a287cdfbd0e22caf5d65051f12a3a4
Jaroslaw1994s <jaroslaw94kotlorz@gmail.com>
2021-07-18 19:05:04 +0200

    Dodanie pobierania z bazy danych do obu funkcji

--- src/Controller/CurrencyController.php
+++ src/Controller/CurrencyController.php
@@ -7,17 +7,22 @@ use SimpleXMLElement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 
 class CurrencyController extends AbstractController
 {
     /**
      * @Route("/synchronize", name="synchronize")
      */
-    public function synchronize(): Response
+    public function synchronize(EntityManagerInterface $em): Response
     {
         $xmlString = file_get_contents("http://api.nbp.pl/api/exchangerates/tables/A/?format=xml");
         $xml = new SimpleXMLElement($xmlString);
         $rates = $xml->ExchangeRatesTable[0]->Rates[0]->Rate;
+        $repository = $em->getRepository(Currency::class);
+        $currencyAll = $repository->findAll();
+        $entityManager = $this->getDoctrine()->getManager();
 
         foreach($rates as $rate)
         {
@@ -25,20 +30,24 @@ class CurrencyController extends AbstractController
             $currencyCode = $rate->Code;
             $exchangeRateFloat = $rate->Mid;
             $exchangeRate = (float)$exchangeRateFloat*10000;
+
             $CurrencySYNC = new Currency($name, $currencyCode, $exchangeRate);
         }
 
 
         return $this->render('currency/index.html.twig', [
             'controller_name' => 'CurrencyController',
-            var_dump($CurrencySYNC)
         ]);
     }
     /**
      * @Route("/currency", name="currency")
      */
-    public function display(): Response
+    public function display(EntityManagerInterface $em): Response
     {
+        $repository = $em->getRepository(Currency::class);
+        $currencyAll = $repository->findAll();
+        $entityManager = $this->getDoctrine()->getManager();
+
         return $this->render('currency/index.html.twig', [
             'controller_name' => 'CurrencyController',
         ]);
